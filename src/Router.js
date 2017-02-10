b0bc87f2596c7ba9cad602a74a5f129ca92c892f
vibecode <rumashkin@gmail.com>
2017-02-10 19:13:28 +0100

    add employee form component

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import CreateEmployee from './components/CreateEmployee';
+import { Actions } from 'react-native-router-flux';
 
 const RouterComponent = () => {
   return (
@@ -16,9 +18,16 @@ const RouterComponent = () => {
 
         <Scene key="main">
           <Scene
+              onRight={() => Actions.createEmployee()}
+              rightTitle="Add"
               key="employeeList"
               component={EmployeeList}
               title="Employees"
+          />
+          <Scene
+              key="createEmployee"
+              title="Create Employee"
+              component={CreateEmployee}
           />
         </Scene>
       </Router>
---
+++ src/components/CreateEmployee.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class CreateEmployee extends Component {
+
+  render() {
+    return (
+        <View>
+          <Text>
+            Employee form
+          </Text>
+        </View>
+    )
+  }
+}
+
+export default CreateEmployee;
