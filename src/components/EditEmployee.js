ad9ca55c5906a7da7c00538e1ee6a5bcdd62b413
vibecode <rumashkin@gmail.com>
2017-02-15 22:38:20 +0100

    add messaging functionality

--- src/components/EditEmployee.js
+++ src/components/EditEmployee.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
+import Communications from 'react-native-communications';
 import EmployeeForm from './EmployeeForm';
 import { updateEmployee, saveEmployee } from '../actions';
 import { Card, Button } from './common';
+import { View } from 'react-native';
 import { connect } from 'react-redux';
 
 class EditEmployee extends Component {
   constructor(props) {
     super(props);
     this.onButtonPress = this.onButtonPress.bind(this);
+    this.onTextPress = this.onTextPress.bind(this);
   }
 
   componentWillMount() {
@@ -23,14 +26,28 @@ class EditEmployee extends Component {
     this.props.saveEmployee({name, phone, shift, uid: this.props.employee.uid});
   }
 
+  onTextPress() {
+    const {phone, shift} = this.props;
+
+    Communications.text(phone, `Your upcoming shift is on ${shift}`);
+  }
+
   render() {
     return (
-        <Card>
-          <EmployeeForm />
-          <Button onPress={this.onButtonPress}>
-            Save Changes
-          </Button>
-        </Card>
+        <View>
+          <Card>
+            <EmployeeForm />
+            <Button onPress={this.onButtonPress}>
+              Save Changes
+            </Button>
+          </Card>
+
+          <Card>
+            <Button onPress={this.onTextPress}>
+              Send Message
+            </Button>
+          </Card>
+        </View>
     )
   }
 }
